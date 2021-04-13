a7585f825616518234e4c973ee49b6181eb157c0
Div685 <divyeshpatel685@gmail.com>
2021-04-13 22:05:59 +0300

    Add style to the buttons
    - made all buttons equal size

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -12,25 +12,25 @@ export default class ButtonPanel extends Component {
           <Button name="%" />
           <Button name="÷" />
         </div>
-        <div lassName="group-2">
+        <div className="group-2">
           <Button name="7" />
           <Button name="8" />
           <Button name="9" />
           <Button name="X" />
         </div>
-        <div lassName="group-3">
+        <div className="group-3">
           <Button name="4" />
           <Button name="5" />
           <Button name="6" />
           <Button name="-" />
         </div>
-        <div>
+        <div className="group-4">
           <Button name="1" />
           <Button name="2" />
           <Button name="3" />
           <Button name="+" />
         </div>
-        <div>
+        <div className="group-5">
           <Button name="0" />
           <Button name=" . " />
           <Button name="=" />
