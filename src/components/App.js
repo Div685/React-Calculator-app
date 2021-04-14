b954b14042b60ba72a7f6ae94fadaf7653f3bb75
Div685 <divyeshpatel685@gmail.com>
2021-04-14 16:06:21 +0300

    import calculate function to the App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
+// eslint-disable-next-line no-unused-vars
 import calculate from '../logic/calculate';
 
 function App() {
@@ -11,7 +12,5 @@ function App() {
     </React.Fragment>
   );
 }
-
-calculate();
 
 export default App;
