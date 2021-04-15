62c540b10d128d9f3a9119a7911480e7d2eb40b5
Div685 <divyeshpatel685@gmail.com>
2021-04-15 18:38:41 +0300

    update functional component to class component

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,35 @@ import Display from './Display';
 // eslint-disable-next-line no-unused-vars
 import calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <React.Fragment key="key">
-      <Display />
-      <ButtonPanel name="Button" />
-    </React.Fragment>
-  );
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Display />
+        <ButtonPanel name="Button" />
+      </div>
+    );
+  }
 }
 
 export default App;
+
+// function App() {
+//   return (
+//     <React.Fragment key="key">
+//       <Display />
+//       <ButtonPanel name="Button" />
+//     </React.Fragment>
+//   );
+// }
+
+// export default App;
