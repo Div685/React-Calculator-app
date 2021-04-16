63f777934bfac75ec978773a6b1df8d32986a2d2
Div685 <divyeshpatel685@gmail.com>
2021-04-16 10:57:13 +0300

    change class to stateless component

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,22 +1,29 @@
-/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class Display extends React.Component {
-  render() {
-    const { result } = this.props;
-    return (
-      <div>
-        <h1>{ result }</h1>
-      </div>
-    );
-  }
-}
+const Display = ({ total, next }) => (
+  <div className="display">
+    <span>{(next && next.toString()) || (total && total.toString()) || '0'}</span>
+    {console.log(total, next)}
+  </div>
+);
+// class Display extends React.Component {
+//   render() {
+//     const { result } = this.props;
+//     console.log(result);
+//     return (
+//       <div>
+//         <h1>{ result }</h1>
+//       </div>
+//     );
+//   }
+// }
 
 Display.propTypes = {
-  result: PropTypes.string,
+  total: PropTypes.string,
+  next: PropTypes.string,
 };
 
-Display.defaultProps = { result: '0' };
+Display.defaultProps = { total: '', next: '' };
 
 export default Display;
