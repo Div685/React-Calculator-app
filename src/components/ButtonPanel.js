7f3298222cbcc1c895529bf644fcde4d06fafe77
Div685 <divyeshpatel685@gmail.com>
2021-04-16 13:09:09 +0300

    update button panel file
    - class based to functionbased to remove linter error of stateless component

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,48 +1,49 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Button from './Button';
 
-// eslint-disable-next-line react/prefer-stateless-function
-export default class ButtonPanel extends Component {
-  render() {
-    const { clickHandler } = this.props;
-    // console.log('button Panel:', clickHandler);
-    return (
-      <div className="main-div">
-        <div className="group-1">
-          <Button name="AC" clickHandler={clickHandler} />
-          <Button name="+/-" clickHandler={clickHandler} />
-          <Button name="%" />
-          <Button name="÷" />
-        </div>
-        <div className="group-2">
-          <Button name="7" />
-          <Button name="8" />
-          <Button name="9" />
-          <Button name="X" />
-        </div>
-        <div className="group-3">
-          <Button name="4" />
-          <Button name="5" />
-          <Button name="6" />
-          <Button name="-" />
-        </div>
-        <div className="group-4">
-          <Button name="1" />
-          <Button name="2" />
-          <Button name="3" />
-          <Button name="+" />
-        </div>
-        <div className="group-5">
-          <Button name="0" />
-          <Button name=" . " />
-          <Button name="=" />
-        </div>
+function ButtonPanel({ clickHandler }) {
+  return (
+    <div className="main-div">
+      <div className="group-1">
+        <Button clickHandler={clickHandler} name="AC" />
+        <Button name="+/-" clickHandler={clickHandler} />
+        <Button name="%" clickHandler={clickHandler} />
+        <Button name="÷" clickHandler={clickHandler} />
       </div>
-    );
-  }
+      <div className="group-2">
+        <Button name="7" clickHandler={clickHandler} />
+        <Button name="8" clickHandler={clickHandler} />
+        <Button name="9" clickHandler={clickHandler} />
+        <Button name="x" clickHandler={clickHandler} />
+      </div>
+      <div className="group-3">
+        <Button name="4" clickHandler={clickHandler} />
+        <Button name="5" clickHandler={clickHandler} />
+        <Button name="6" clickHandler={clickHandler} />
+        <Button name="-" clickHandler={clickHandler} />
+      </div>
+      <div className="group-4">
+        <Button name="1" clickHandler={clickHandler} />
+        <Button name="2" clickHandler={clickHandler} />
+        <Button name="3" clickHandler={clickHandler} />
+        <Button name="+" clickHandler={clickHandler} />
+      </div>
+      <div className="group-5">
+        <Button name="0" clickHandler={clickHandler} />
+        <Button name="." clickHandler={clickHandler} />
+        <Button name="=" clickHandler={clickHandler} />
+      </div>
+    </div>
+  );
 }
 
 ButtonPanel.propTypes = {
-  clickHandler: PropTypes.func.isRequired,
+  clickHandler: PropTypes.func,
 };
+
+ButtonPanel.defaultProps = {
+  clickHandler: null,
+};
+
+export default ButtonPanel;
