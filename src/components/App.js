70279e7cec8c91b4b0a9121c7ce077bd11c35246
Div685 <divyeshpatel685@gmail.com>
2021-04-18 22:52:09 +0300

    change App component from class to Function
    - to use Hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,41 @@
 import React from 'react';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
-import calculate from '../logic/calculate';
+// import calculate from '../logic/calculate';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: '',
-      next: '',
-      operation: null,
-    };
+const App = () => (
+  <div className="main-calc">
+    <Display />
+    <ButtonPanel />
+  </div>
+);
 
-    this.handleClick = this.handleClick.bind(this);
-  }
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       total: '',
+//       next: '',
+//       operation: null,
+//     };
 
-  handleClick(buttonName) {
-    const newState = calculate(this.state, buttonName);
-    this.setState(() => ({ ...newState }));
-  }
+//     this.handleClick = this.handleClick.bind(this);
+//   }
 
-  render() {
-    const { total, next } = this.state;
-    return (
-      <div className="main-calc">
-        <Display total={total} next={next} />
-        <ButtonPanel clickHandler={this.handleClick} />
-      </div>
-    );
-  }
-}
+//   handleClick(buttonName) {
+//     const newState = calculate(this.state, buttonName);
+//     this.setState(() => ({ ...newState }));
+//   }
+
+//   render() {
+//     const { total, next } = this.state;
+//     return (
+//       <div className="main-calc">
+//         <Display total={total} next={next} />
+//         <ButtonPanel clickHandler={this.handleClick} />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
