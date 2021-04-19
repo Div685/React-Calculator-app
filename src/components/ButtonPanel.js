6e4df9e7c7905d86aaa26684b0cda6b19e34bacf
Div685 <divyeshpatel685@gmail.com>
2021-04-19 16:38:38 +0300

    Add style files
    - Dispaly.css, App.css, ButtonPanel.css

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from './Button';
+import '../style/ButtonPanel.css';
 
 function ButtonPanel({ clickHandler }) {
   return (
