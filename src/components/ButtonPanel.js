82628b5cd9137e2013c07039da2c958ebf51dcfc
Div685 <divyeshpatel685@gmail.com>
2021-04-19 22:36:22 +0300

    Add background color and wide props
    - to change background color of the button
    - to make 0 width size 50%

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -4,36 +4,37 @@ import Button from './Button';
 import '../style/ButtonPanel.css';
 
 function ButtonPanel({ clickHandler }) {
+  // const orangeButton = ['÷', 'x', '-', '+', '='];
   return (
     <div className="main-div">
       <div className="group-row">
         <Button clickHandler={clickHandler} name="AC" />
         <Button name="+/-" clickHandler={clickHandler} />
         <Button name="%" clickHandler={clickHandler} />
-        <Button name="÷" clickHandler={clickHandler} />
+        <Button name="÷" clickHandler={clickHandler} color="#F5913E" />
       </div>
       <div className="group-row">
         <Button name="7" clickHandler={clickHandler} />
         <Button name="8" clickHandler={clickHandler} />
         <Button name="9" clickHandler={clickHandler} />
-        <Button name="x" clickHandler={clickHandler} />
+        <Button name="x" clickHandler={clickHandler} color="#F5913E" />
       </div>
       <div className="group-row">
         <Button name="4" clickHandler={clickHandler} />
         <Button name="5" clickHandler={clickHandler} />
         <Button name="6" clickHandler={clickHandler} />
-        <Button name="-" clickHandler={clickHandler} />
+        <Button name="-" clickHandler={clickHandler} color="#F5913E" />
       </div>
       <div className="group-row">
         <Button name="1" clickHandler={clickHandler} />
         <Button name="2" clickHandler={clickHandler} />
         <Button name="3" clickHandler={clickHandler} />
-        <Button name="+" clickHandler={clickHandler} />
+        <Button name="+" clickHandler={clickHandler} color="#F5913E" />
       </div>
       <div className="group-row">
-        <Button name="0" clickHandler={clickHandler} />
+        <Button name="0" clickHandler={clickHandler} wide />
         <Button name="." clickHandler={clickHandler} />
-        <Button name="=" clickHandler={clickHandler} />
+        <Button name="=" clickHandler={clickHandler} color="#F5913E" />
       </div>
     </div>
   );
