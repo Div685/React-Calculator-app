fb5293187885953a63bcb5c8d8e1b20e7242d0fe
Div685 <divyeshpatel685@gmail.com>
2021-04-20 13:31:06 +0300

    update style to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../style/Home.css';
 
 function Home() {
   return (
--- src/components/Quote.js
+++ src/components/Quote.js
@@ -5,7 +5,9 @@ function Quote() {
   return (
     <div className="quote">
       <h1>
-        Mathematics is not about numbers, equations, computations, or
+        Mathematics is not about numbers, equations, computations,
+        <br />
+        or
         algorithms: it is about understanding. –William Paul Thurston
       </h1>
     </div>
